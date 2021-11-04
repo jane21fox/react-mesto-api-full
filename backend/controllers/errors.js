9ee6e0905aa5c58011dbdec98656dd6806e1d2a5
Evgeniya Ukhova <jane21fox@gmail.com>
2021-11-04 22:38:56 +0300

    fix: устранение замечаний 1

--- backend/controllers/errors.js
+++ backend/controllers/errors.js
@@ -1,17 +1,11 @@
 const handleErrors = (err, req, res, next) => {
-  const { statusCode = 500, message, name } = err;
-  if (name === 'ValidationError') {
-    res.status(400)
-      .send({
-        // message: Object.values(err.errors).map((key) => err.errors[key].message).join(),
-        message,
-      });
-  } else {
-    res.status(statusCode)
-      .send({
-        message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
-      });
-  }
+  const { statusCode = 500, message } = err;
+  res.status(statusCode)
+    .send({
+      message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
+    });
+
+  return next();
 };
 
 module.exports = {
